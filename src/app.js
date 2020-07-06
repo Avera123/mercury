1394b0f6f685f32ec0cd9bd84d5c88fc5147a73c
Alejandro Vera Carrasquilla <avera02@misena.edu.co>
2020-07-05 19:03:11 -0500

    Add simple middlewars and rutes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res) => res.redirect('/mercury/api/users'));
+app.get('/mercury/api/users', (req, res) => res.send('TST'));
+app.get('/mercury/api/notes', (req, res) => res.send('TST'));
+app.get('/mercury/api/tasks', (req, res) => res.send('TST'));
 
 module.exports = app;
